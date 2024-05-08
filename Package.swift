9a5457ed17bbfe9f52a43c90eec534d898f79ae2
KimWoon <kw31120@naver.com>
2024-05-08 14:21:17 +0900

    오류 수정

--- Package.swift
+++ Package.swift
@@ -14,8 +14,7 @@ let package = Package(
     
     dependencies: [
         .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.7.1"))
-    ]
-    
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
